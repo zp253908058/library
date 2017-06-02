6933b8caa1e4f7383ad241dba7bb3e26e9acfe67
zp <253908058@qq.com>
2017-06-02 19:22:28 +0800

    null

---
+++ app/src/main/java/com/future/library/MainActivity.java
@@ -0,0 +1,15 @@
+package com.future.library;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Class description:
+ *
+ * @author zp
+ * @version 1.0
+ * @see MainActivity
+ * @since 2017/6/2
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
